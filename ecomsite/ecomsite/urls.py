c72833c4b5fe681171852f34333bcf70fcda7879
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-09-16 16:13:54 +0300

    pop on and check out page

--- ecomsite/ecomsite/urls.py
+++ ecomsite/ecomsite/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from shop.views import index, detail
+from shop.views import index, detail,checkout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index, name='index'),
     path('<int:id>/',detail, name='detail'),
-    
+    path('checkout/',checkout, name='checkout'),
 ]
